29c288eaad416da8b410f188d1e9e229f700411b
kiet12 <77476352+kiet12@users.noreply.github.com>
2021-07-13 12:01:51 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DauSonNam_Buoi3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeQuocKiet_1811062753.Startup))]
+namespace LeQuocKiet_1811062753
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
